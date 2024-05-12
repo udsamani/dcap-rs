4590e8db887a123bc7394b061b5aa6882e2dfffd
Chua Zheng Leong <melynx@gmail.com>
2024-05-12 13:35:55 +0800

    Add IntelCollateralV3.
    Data structure to help with the passing and managing collaterals.
    Environment like object.

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,4 +1,9 @@
 use serde::{Serialize, Deserialize};
+use x509_parser::{certificate::X509Certificate, revocation_list::CertificateRevocationList};
+
+use crate::utils::cert::{parse_der, parse_der_multi, pem_to_der};
+
+use self::{enclave_identity::EnclaveIdentityV2, tcbinfo::TcbInfoV2};
 
 pub mod quote;
 pub mod tcbinfo;
@@ -19,11 +24,110 @@ pub enum TcbStatus {
 
 #[derive(Clone, Debug)]
 pub struct IntelCollateralV3 {
-    pub tcbinfov2_json: String,
-    pub qe_identityv2_json: String,
-    pub intel_root_ca_cert: Vec<u8>,
-    pub sgx_tcb_signing_cert: Vec<u8>,
-    pub sgx_pck_cert_chain: Option<Vec<u8>>,
+    pub tcbinfov2: Option<TcbInfoV2>,
+    pub qe_identityv2: Option<EnclaveIdentityV2>,
+    pub intel_root_ca_der: Option<Vec<u8>>,
+    pub sgx_tcb_signing_der: Option<Vec<u8>>,
+    pub sgx_pck_certchain_der: Option<Vec<u8>>,
+    pub sgx_intel_root_ca_crl_der: Option<Vec<u8>>,
+    pub sgx_pck_processor_crl_der: Option<Vec<u8>>,
+    pub sgx_pck_platform_crl_der: Option<Vec<u8>>,
+}
+
+// builder pattern for IntelCollateralV3
+impl IntelCollateralV3 {
+    pub fn new() -> IntelCollateralV3 {
+        IntelCollateralV3 {
+            tcbinfov2: None,
+            qe_identityv2: None,
+            intel_root_ca_der: None,
+            sgx_tcb_signing_der: None,
+            sgx_pck_certchain_der: None,
+            sgx_intel_root_ca_crl_der: None,
+            sgx_pck_processor_crl_der: None,
+            sgx_pck_platform_crl_der: None,
+        }
+    }
+
+    pub fn set_tcbinfov2(&mut self, tcbinfov2_slice: &[u8]) {
+        self.tcbinfov2 = serde_json::from_slice(tcbinfov2_slice).unwrap();
+    }
+
+    pub fn set_qeidentityv2(&mut self, qeidentityv2_slice: &[u8]) {
+        self.qe_identityv2 = serde_json::from_slice(qeidentityv2_slice).unwrap();
+    }
+
+    pub fn get_intel_root_ca<'a>(&'a self) -> X509Certificate<'a> {
+        match self.intel_root_ca_der {
+            Some(ref der) => {
+                let cert = parse_der(der);
+                cert
+            },
+            None => panic!("Intel Root CA not set"),
+        }
+    }
+
+    pub fn set_intel_root_ca_der(&mut self, intel_root_ca_der: &[u8]) {
+        self.intel_root_ca_der = Some(intel_root_ca_der.to_vec());
+    }
+
+    pub fn get_sgx_tcb_signing<'a>(&'a self) -> X509Certificate<'a> {
+        match self.sgx_tcb_signing_der {
+            Some(ref der) => {
+                let cert = parse_der(der);
+                cert
+            },
+            None => panic!("SGX TCB Signing Cert not set"),
+        }
+    }
+
+    pub fn set_sgx_tcb_signing_der(&mut self, sgx_tcb_signing_der: &[u8]) {
+        self.sgx_tcb_signing_der = Some(sgx_tcb_signing_der.to_vec());
+    }
+
+    pub fn set_sgx_tcb_signing_pem(&mut self, sgx_tcb_signing_pem: &[u8]) {
+        // convert pem to der
+        let sgx_tcb_signing_der = pem_to_der(sgx_tcb_signing_pem);
+        self.sgx_tcb_signing_der = Some(sgx_tcb_signing_der);
+    }
+
+    pub fn get_sgx_pck_certchain<'a>(&'a self) -> Option<Vec<X509Certificate<'a>>> {
+        match &self.sgx_pck_certchain_der {
+            Some(certchain_der) => {
+                let certchain = parse_der_multi(certchain_der);
+                Some(certchain)
+            },
+            None => None,
+        }
+    }
+
+    pub fn set_sgx_pck_certchain_der(&mut self, sgx_pck_certchain_der: Option<&[u8]>) {
+        match sgx_pck_certchain_der {
+            Some(certchain_der) => {
+                self.sgx_pck_certchain_der = Some(certchain_der.to_vec());
+            },
+            None => {
+                self.sgx_pck_certchain_der = None;
+            },
+        }
+    }
+
+    pub fn set_sgx_pck_certchain_pem(&mut self, sgx_pck_certchain_pem: Option<&[u8]>) {
+        match sgx_pck_certchain_pem {
+            Some(certchain_pem) => {
+                // convert pem to der
+                let sgx_pck_certchain_der = pem_to_der(certchain_pem);
+                self.sgx_pck_certchain_der = Some(sgx_pck_certchain_der);
+            },
+            None => {
+                self.sgx_pck_certchain_der = None;
+            },
+        }
+    }
+
+    pub fn set_sgx_processor_crl_der(&mut self, sgx_pck_processor_crl_der: &[u8]) {
+        self.sgx_pck_processor_crl_der = Some(sgx_pck_processor_crl_der.to_vec());
+    }
 }
 
 // serialization:
